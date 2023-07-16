0a426d2ad6e0d674531b087c2bf90c85145111ae
EstebanSL <gandhisanchezl00@gmail.com>
2023-07-15 20:13:01 -0500

    Initial project configuration

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,25 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<PublicGuard />}>
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path="*" element={<Navigate to="/login" />} />
+        </Route>
+
+        <Route element={<AuthGuard />}>
+          <Route path='/institutions' element={<Institutions />} />
+          <Route path='/institution/login/:id' element={<LoginInstitution />} />
+          <Route path='/institution/details/:id' element={<InstitutionDetails />} >
+            <Route path='account/:accountId' element={<AccountDetails />} />
+            <Route path="*" element={<Navigate to="/institutions" />} />
+          </Route>
+          <Route path="*" element={<Navigate to="/institutions" />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
